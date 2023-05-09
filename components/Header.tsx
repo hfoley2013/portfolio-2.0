f5360bea5bdc1eb45e6a084eb6e863382705dd8d
Harper Foley <harper.e.foley@gmail.com>
2023-05-09 16:11:28 -0700

    Adds FramerMotion

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,29 @@ type Props = {}
 export default function Header({}: Props) {
   return (
     <>
-      <header>
-        <div>
-          <SocialIcon url="https://twitter.com/jaketrent" />
-        </div>
+      <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+        <div className="flex flex-row items-center">
+          <SocialIcon 
+            url="https://twitter.com/RainierRacingCo"
+            fgColor='gray'
+            bgColor='transparent'  
+          />
+          <SocialIcon 
+            url="https://linkedin.com/in/harper-e-foley"
+            fgColor='gray'
+            bgColor='transparent'  
+          />
+      </div>
+
+          <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+            <SocialIcon 
+              className='cursor-pointer'
+              network='email'
+              fgColor='gray'
+              bgColor='transparent'  
+            />
+            <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
+          </div>
       </header>
     </>
   )
