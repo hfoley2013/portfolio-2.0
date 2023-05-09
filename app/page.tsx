38874dbf42b69dfee363b905d2b80d63e4b6d426
Harper Foley <harper.e.foley@gmail.com>
2023-05-09 16:35:19 -0700

    Completes Header. Adds Hero to components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <div>
       <Header />
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
       {/* About */}
       {/* Experience */}
       {/* Skills */}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
+'use client'
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
@@ -7,9 +9,28 @@ export default function Header({}: Props) {
   return (
     <>
       <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <div className="flex flex-row items-center">
+        <motion.div 
+          initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+          }}
+          transition={{
+            duration: 1.5
+          }}
+          className="flex flex-row items-center">
           <SocialIcon 
             url="https://twitter.com/RainierRacingCo"
+            fgColor='gray'
+            bgColor='transparent'  
+          />
+          <SocialIcon 
+            url="https://www.youtube.com/channel/UCmhcoR5U_9u_OBZj-CmMQqw"
             fgColor='gray'
             bgColor='transparent'  
           />
@@ -18,9 +39,28 @@ export default function Header({}: Props) {
             fgColor='gray'
             bgColor='transparent'  
           />
-      </div>
+          <SocialIcon 
+            url="https://www.github.com/hfoley2013"
+            fgColor='gray'
+            bgColor='transparent'  
+          />
+        </motion.div>
 
-          <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+          <motion.div 
+            initial={{
+              x: 500,
+              opacity: 0,
+              scale: 0.5
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              scale: 1
+            }}
+            transition={{
+              duration: 2
+            }}
+            className="flex flex-row items-center text-gray-300 cursor-pointer">
             <SocialIcon 
               className='cursor-pointer'
               network='email'
@@ -28,7 +68,7 @@ export default function Header({}: Props) {
               bgColor='transparent'  
             />
             <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-          </div>
+          </motion.div>
       </header>
     </>
   )
