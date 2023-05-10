1263c57ec1cd77ff21ee0273274eb24b4c0744a7
Harper Foley <harper.e.foley@gmail.com>
2023-05-10 16:19:59 -0700

    Fixes layering of background and links.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {Cursor, useTypewriter} from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {};
 
@@ -27,12 +28,20 @@ export default function Hero({}: Props) {
         height={256}
         className="relative rounded-full h-32 w-32 max-w-xs max-h-xs mx-auto object-cover"
       />
-      <div>
+      <div className="z-10">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Full Stack Developer</h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-4xl lg:text-5xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
+
+        <div className="pt-5">
+          <Link href="#" className="heroButton">About</Link>
+          <Link href="#" className="heroButton">Experience</Link>
+          <Link href="#" className="heroButton">Skills</Link>
+          <Link href="#" className="heroButton">Projects</Link>
+          <Link href="#" className="heroButton">Resume</Link>
+        </div>
       </div>
     </div>
   );
