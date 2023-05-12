e97626084c9dba35d64ec20c273b971077ed1a4f
Harper Foley <harper.e.foley@gmail.com>
2023-05-11 19:37:19 -0700

    Adds Project component.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Experience from '../components/Experience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 export default function Home() {
   return (
@@ -24,7 +25,11 @@ export default function Home() {
       <section id="skills" className="snap-center">
         <Skills />
       </section>
-      
+
+      <section id="projects" className="snap-center">
+        <Projects />
+      </section>
+
     </div>
   );
 }
