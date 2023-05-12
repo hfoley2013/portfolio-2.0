0e2a2ee78dbad37af4a26190220a0d13416e5eb0
Harper Foley <harper.e.foley@gmail.com>
2023-05-12 09:31:18 -0700

    Installs Sanity. Installs Tailwind Scrollbar. Installs Sanity CLI. Defines Sanity Studio routes in app directory.

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import Experience from '../components/Experience'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import ContactMe from '../components/ContactMe'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header />
       
       <section id="hero" className="snap-start">
@@ -34,6 +36,21 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image 
+              src="/RainierRanch.png"
+              alt="Footer Logo"
+              height={128}
+              width={128}
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            />
+          </div>
+        </footer>
+      </Link>
+
     </div>
   );
 }
