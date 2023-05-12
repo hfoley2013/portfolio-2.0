7cf5c5736e5d825b2f4474f9b9a4bf9aace8c8e1
Harper Foley <harper.e.foley@gmail.com>
2023-05-11 21:51:03 -0700

    Fixes Skills floating to top of screen.

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,7 +11,7 @@ function Experience({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="relative flex flex-col items-center h-screen max-w-full px-10 mx-auto overflow-hidden text-lft md:flex-row justify-evenly"
+      className="relative flex flex-col items-center h-screen max-w-full px-10 mx-auto overflow-hidden text-left md:flex-row justify-evenly"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
