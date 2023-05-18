655ab6959c63dd523c6a369b4cb83a1de3122881
Harper Foley <harper.e.foley@gmail.com>
2023-05-17 21:31:03 -0700

    Connects CMS to Header and Hero components.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,13 +4,17 @@ import {Cursor, useTypewriter} from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import Image from 'next/image';
 import Link from 'next/link';
+import { PageInfo } from '@/typings';
+import { urlForImage } from '@/sanity/lib/image';
 
-type Props = {};
+type Props = {
+  pageInfo: PageInfo
+};
 
-export default function Hero({}: Props) {
+export default function Hero({ pageInfo }: Props) {
   const [text, count] = useTypewriter({
     words: [
-      "Hi, my name is Harper Foley.",
+      `Hi, my name is ${pageInfo.name}.`,
       "Bombs => Banking => Backend",
       "Full Stack Dev",
     ],
@@ -22,15 +26,15 @@ export default function Hero({}: Props) {
     <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
       <BackgroundCircles />
       <Image
-        src="/headshot.jpg"
-        alt="Profile Image of Harper Foley"
+        src={urlForImage(pageInfo.heroImage.asset).url()}
+        alt={`Profile Image of ${pageInfo.name}`}
         width={256}
         height={256}
         className="relative object-cover w-32 h-32 mx-auto rounded-full"
         priority
       />
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Full Stack Developer</h2>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">{pageInfo.role}</h2>
         <h1 className="px-10 text-5xl font-semibold lg:text-6xl">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
