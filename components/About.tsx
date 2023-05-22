111b57b472451cff9aaa29710b7db8db391eef54
Harper Foley <harper.e.foley@gmail.com>
2023-05-22 12:03:44 -0700

    Improves responsive design in Hero and About

--- components/About.tsx
+++ components/About.tsx
@@ -15,36 +15,40 @@ function About({ pageInfo }: Props) {
       transition={{ duration: 1.5 }}
       className="relative flex flex-col items-center h-screen px-10 mx-auto text-center md:text-left md:flex-row max-w-7xl justify-evenly"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl w-full">
         About
       </h3>
-    
-      <motion.img 
-        initial={{
-          x: -200,
-          opacity: 0,
-        }}
-        transition={{
-          duration: 1.2,
-        }}
-        whileInView={{ 
-          x: 0,
-          opacity: 1,
-         }}
-        viewport={{ once: true }}
-        src="/headshot.jpg"
-        className="flex-shrink-0 object-cover w-56 h-56 mb-20 rounded-full md:mb-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
-      />
+      <div className="flex flex-col sm:flex-row md:item-center">
+        <motion.img 
+          initial={{
+            x: -200,
+            opacity: 0,
+          }}
+          transition={{
+            duration: 1.2,
+          }}
+          whileInView={{ 
+            x: 0,
+            opacity: 1,
+          }}
+          viewport={{ once: true }}
+          src="/headshot.jpg"
+          // className="flex-shrink-0 object-cover max-w-full h-56 mb-8 rounded-full md:mb-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+          className="object-cover h-32 w-32 mb-8 rounded-full md:mb-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+          // className="flex-shrink-1 object-cover max-w-full h-32 sm:h-56 mb-8 rounded-full sm:mb-0 sm:mr-8 md:mr-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+          // className="object-cover w-32 h-32 sm:h-auto mb-8 rounded-full sm:mb-0 sm:mr-8 md:mr-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+        />
 
-      <div className="px-0 space-y-10 md:px-10">
-        <h4 className="text-4xl font-semibold">
-          Here is a{" "}
-          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
-          background
-        </h4>
-        <p className="text-base">
-          {pageInfo?.backgroundInformation}
-        </p>
+        <div className="flex flex-col space-y-5 md:ml-8">
+          <h4 className="text-base font-semibold sm:text-4xl">
+            Here is a{" "}
+            <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+            background
+          </h4>
+          <p className="text-xs text-left sm:text-base">
+            {pageInfo?.backgroundInformation}
+          </p>
+        </div>
       </div>
     </motion.div>
   )
