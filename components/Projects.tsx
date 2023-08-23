3182ba6c83fa5077f5427e1606d12c7f7e29d4b0
Harper Foley <harper.e.foley@gmail.com>
2023-08-23 11:52:05 -0700

    Fixes to projects (WIP)

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -24,7 +24,7 @@ function Projects({ projects }: Props) {
 
       <div className="relative z-20 flex w-full overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {projects?.map((project, i) => (
-          <div key={project._id} className="flex flex-col items-center justify-center flex-shrink-0 w-screen h-screen p-20 space-y-5 snap-center md:p-44">
+          <div key={project._id} className="flex flex-col items-center justify-center flex-shrink-0 w-screen h-screen p-4 space-y-5 sm:p-5 snap-center md:p-20 lg:p-44">
             <motion.img
               initial={{
                 y: -300,
@@ -35,11 +35,11 @@ function Projects({ projects }: Props) {
               viewport={{ once: true }}
               src={urlForImage(project?.image.asset).url()}
               alt={project.title}
-              className=""
+              className="fill"
             />
 
-            <div className="max-w-6xl px-0 space-y-10 md:px-10">
-              <h4 className="text-4xl font-semibold text-center">
+            <div className="max-w-6xl px-0 space-y-2 sm:space-y-4 md:space-y-6 lg:space-y-10 md:px-10">
+              <h4 className="text-lg font-semibold text-center sm:text-xl md:text-2xl lg:text-4xl">
                 <span className="underline decoration-[#F7AB0A]/50">
                   Case Study {i + 1} of {projects?.length}:
                 </span>{" "}
@@ -54,12 +54,12 @@ function Projects({ projects }: Props) {
                     alt={technology.title}
                     width={250}
                     height={250}
-                    className="w-5 h-5"
+                    className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20"
                   />
                 ))}
               </div>
 
-              <p className="text-lg text-center md:text-left">
+              <p className="text-sm text-left sm:text-base md:text-lg">
                 {project?.summary}
               </p>
             </div>
