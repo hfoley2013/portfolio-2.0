b291be14cfd52bcde65accf19b57517230550620
Harper E. Foley <109574453+hfoley2013@users.noreply.github.com>
2023-08-23 19:00:11 -0700

    Fixed miss > in framer motion

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,6 +18,7 @@ function Projects({ projects }: Props) {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       className="relative z-0 flex flex-col items-center h-screen max-w-full mx-auto overflow-hidden text-left md:flex-row justify-evenly"
+    >
 
       <h3 className="sectionHeader">
         Projects
